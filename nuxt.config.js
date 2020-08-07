b567face438c1dd0998390516ed2740bada998f9
Wonder Diaz <wonderdxk@gmail.com>
2020-08-07 17:17:53 -0400

    1.0.17
    update nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,147 +1,156 @@
-
 export default {
-  mode: 'universal',
-  target: 'static',
+  mode: "universal",
+  target: "static",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon-ms.ico' },
-      { rel: 'alternate', hreflang: 'en', href: 'http://metalsyntax.now.sh/' },
-      { rel: 'alternate', hreflang: 'es', href: 'http://metalsyntax.now.sh/es' },
-      { rel: 'alternate', hreflang: 'pt', href: 'http://metalsyntax.now.sh/pt' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon-ms.ico" },
+      { rel: "alternate", hreflang: "en", href: "http://metalsyntax.now.sh/" },
+      {
+        rel: "alternate",
+        hreflang: "es",
+        href: "http://metalsyntax.now.sh/es"
+      },
+      { rel: "alternate", hreflang: "pt", href: "http://metalsyntax.now.sh/pt" }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  *
-  */
- router: {
-  middleware: 'i18n',
-  linkActiveClass: 'active-link',
-  linkExactActiveClass: 'exact-active-link'
+   *
+   */
+  router: {
+    middleware: "i18n",
+    linkActiveClass: "active-link",
+    linkExactActiveClass: "exact-active-link"
   },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    '~/plugins/i18n.js',
-    '~/plugins/typed.js',
-    {src: "@/plugins/aos", ssr: false}
+    "~/plugins/i18n.js",
+    "~/plugins/typed.js",
+    { src: "@/plugins/aos", ssr: false }
   ],
   /*
-  ** Generates
-  */
+   ** Generates
+   */
   generate: {
-    dir: 'public',
+    dir: "public",
     routes: [
-    '/',
-    '/portfolio',
-    '/achievements',
-    '/experience',
-    '/es',
-    '/es/portfolio',
-    '/es/achievements',
-    '/es/experience',
-    '/pt',
-    '/pt/portfolio',
-    '/pt/achievements',
-    '/pt/experience'
-  ]
+      "/",
+      "/portfolio",
+      "/achievements",
+      "/experience",
+      "/es",
+      "/es/portfolio",
+      "/es/achievements",
+      "/es/experience",
+      "/pt",
+      "/pt/portfolio",
+      "/pt/achievements",
+      "/pt/experience"
+    ]
   },
   /*
-  ** Detect Browser Language
-  */
+   ** Detect Browser Language
+   */
   detectBrowserLanguage: {
     useCookie: true,
-    cookieKey: 'i18n_redirected'
+    cookieKey: "i18n_redirected"
   },
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss','@nuxtjs/google-analytics','@nuxtjs/pwa',
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-analytics",
+    "@nuxtjs/pwa"
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
-    '@nuxtjs/robots','@nuxtjs/sitemap',//['@nuxtjs/pwa', {icon: false }],
+    "@nuxtjs/robots",
+    "@nuxtjs/sitemap" //['@nuxtjs/pwa', {icon: false }],
   ],
   /*
-  *
-  */
- pwa: {
-  manifest: {
-    name: 'Wonder Diaz Portfolio. MetalSyntax',
-    short_name: 'MetalSyntax PWA',
-    background_color: "#000",
-    description: "Wonder Diaz Portfolio. Develop by MetalSyntax with Vue.js and TailWindCss.",
-    lang: 'en',
-    display: 'standalone',
+   *
+   */
+  pwa: {
+    manifest: {
+      name: "Wonder Diaz Portfolio. MetalSyntax",
+      short_name: "MetalSyntax PWA",
+      background_color: "#000",
+      description:
+        "Wonder Diaz Portfolio. Develop by MetalSyntax with Vue.js and TailWindCss.",
+      lang: "en",
+      display: "standalone"
     }
   },
   /*
-  *
-  */
+   *
+   */
   googleAnalytics: {
-    id: 'UA-86102577-2'
+    id: "UA-86102577-2"
   },
   /*
    * Sitemap
-  */
- sitemap: {
-  hostname: 'http://metalsyntax.now.sh/',
-  routes: [
-    '/',
-    '/portfolio',
-    '/achievements',
-    '/experience',
-    '/es',
-    '/es/portfolio',
-    '/es/achievements',
-    '/es/experience',
-    '/pt',
-    '/pt/portfolio',
-    '/pt/achievements',
-    '/pt/experience'
-  ],
-  path: '/sitemap.xml',
-  gzip: true,
-  generate: false,
+   */
+  sitemap: {
+    hostname: "http://metalsyntax.now.sh/",
+    routes: [
+      "/",
+      "/portfolio",
+      "/achievements",
+      "/experience",
+      "/es",
+      "/es/portfolio",
+      "/es/achievements",
+      "/es/experience",
+      "/pt",
+      "/pt/portfolio",
+      "/pt/achievements",
+      "/pt/experience"
+    ],
+    path: "/sitemap.xml",
+    gzip: true,
+    generate: false
   },
   /*
-  *
-  */
+   *
+   */
   robots: {
     /*UserAgent: '*',
     Disallow: '/'*/
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    vendor: ['vue-i18n'],
-    extend (config, ctx) {
-    }
-  },
-}
+     ** You can extend webpack config here
+     */
+    vendor: ["vue-i18n"],
+    extend(config, ctx) {}
+  }
+};
